9c2c14a6aa2bcc89c92331cb34b52d4862245d37
abdullah-fikri <abdullahfikri981@gmail.com>
2025-11-25 07:52:01 +0000

    refactor: separate the swagger function

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require("express");
-const expressJSDocSwagger = require('express-jsdoc-swagger');
-
-const docConfig = {
-    info: {
-      version: '1.0.0',
-      title: 'Backend API',
-    },
-    filePattern: './src/**/*.js',
-    swaggerUIPath: '/api-docs',  
-    baseDir: __dirname,     
-};
+const Routers = require("./src/router/index");
+const doc = require("./src/lib/docs");
 
 const app = express();
-
-expressJSDocSwagger(app)(docConfig);
+doc(app)
 app.use(express.json());
 
-const Routers = require("./src/router/index");
 app.use(Routers);
 
 app.listen(9000, ()=>{
---
+++ src/lib/docs.js
@@ -0,0 +1,16 @@
+const expressJSDocSwagger = require('express-jsdoc-swagger');
+const process = require('node:process')
+function doc(app){
+const docConfig = {
+    info: {
+      version: '1.0.0',
+      title: 'Backend API',
+    },
+    filePattern: './src/**/*.js',
+    swaggerUIPath: '/api-docs',  
+    baseDir:process.cwd(),     
+};
+expressJSDocSwagger(app)(docConfig);
+
+}
+module.exports = doc
